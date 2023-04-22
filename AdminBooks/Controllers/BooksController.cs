61cc38953e9295a221775dff42e5b9e848173ed2
MrGallego <44037673+MrGallego@users.noreply.github.com>
2023-04-22 17:13:47 -0500

    ControllerAPIBook

---
+++ AdminBooks/Controllers/BooksController.cs
@@ -0,0 +1,33 @@
+﻿using AdminBooks.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AdminBooks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet("{isbn}")]
+        public async Task<IActionResult> GetBook(string isbn)
+        {
+            var url = $"http://openlibrary.org/api/books?bibkeys=ISBN:{isbn}&format=json&jscmd=details";
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var book = JsonConvert.DeserializeObject<Books>(json);
+
+                return Ok(book);
+            }
+            else
+            {
+                return StatusCode((int)response.StatusCode, "Ocurrió un error al realizar la peticion.");
+            }
+        }
+    }
+}
---
+++ AdminBooks/Models/Books.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace AdminBooks.Models
+{
+    public class Books
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("authors")]
+        public Author[] Authors { get; set; }
+
+        [JsonProperty("publishers")]
+        public Publisher[] Publishers { get; set; }
+    }
+
+    public class Author
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public class Publisher
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
+
